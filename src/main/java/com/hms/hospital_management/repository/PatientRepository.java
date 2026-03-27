cece0395473dcc2ff8d621fb35dc03ec383339f5
hibbs-byte <alihibban78@gmail.com>
2026-03-27 14:15:19 +0530

    Added Patient Repository

---
+++ src/main/java/com/hms/hospital_management/repository/PatientRepository.java
@@ -0,0 +1,7 @@
+package com.hms.hospital_management.repository;
+
+import com.hms.hospital_management.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+}
