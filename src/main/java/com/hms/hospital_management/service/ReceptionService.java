d0d893bf68a2cb3be23979a8348faf37b4a9486d
Pushpendra8787 <pushpendra99x@gmail.com>
2026-04-01 15:02:54 +0530

    added relation repo , Reception service , receptionServiceImpl, receptionController

--- src/main/java/com/hms/hospital_management/service/ReceptionService.java
+++ src/main/java/com/hms/hospital_management/service/ReceptionService.java
@@ -1,4 +1,17 @@
 package com.hms.hospital_management.service;
 
+import com.hms.hospital_management.dto.response.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ReceptionService {
+
+    List<RoomStatusDTO> getRoomStatus();
+
+    List<AppointmentByDateDTO> getAppointmentsByDate(LocalDate date);
+
+    List<NurseOnCallDTO> getOnCallNurses();
+
+    List<NurseOnCallDTO> getNurses();
 }
